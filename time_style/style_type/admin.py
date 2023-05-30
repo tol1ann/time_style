769095fbb330777c8dad1c760b2212f7e9cf6d8f
vans1r <serebrogml@gmail.com>
2023-05-30 23:51:50 +0300

    some commit

---
+++ time_style/style_type/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Outfit, Category
+
+admin.site.register(Outfit)
+admin.site.register(Category)
---
+++ time_style/style_type/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Категории"""
+    name = models.CharField("Outfit", max_length=150)
+    photo = models.ImageField("Изображение", upload_to="category_of_outfits/")
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Outfit(models.Model):
+    """Аутфиты"""
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    photo = models.ImageField("Изображение", upload_to="outfits/")
+    name = models.CharField('Название', max_length=34, default='asdfasdf')
+
+
+    def __str__(self):
+        return self.name
+
+
+class Thing(models.Model):
+    outfit = models.ForeignKey(Outfit, on_delete=models.CASCADE, verbose_name='Оутфит')
+
