4843734764c4daa35e1734206c47333833875912
tol1an <tolian16995@gmail.com>
2023-06-08 23:05:56 +0300

    FINAL COMMIT. CONGRATULATIONS

--- time_style/style_type/admin.py
+++ time_style/style_type/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Outfit, Category, Thing
 
-admin.site.register(Outfit)
 admin.site.register(Category)
+admin.site.register(Outfit)
 admin.site.register(Thing)
--- time_style/style_type/models.py
+++ time_style/style_type/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     """Категории"""
-    name = models.CharField("Outfit", max_length=150)
-    photo = models.ImageField("Изображение", upload_to="category_of_outfits/")
+    name = models.CharField("Outfit", max_length=150, default='Default Category')
+    photo = models.ImageField("Изображение", null=True, blank=True, upload_to="categories_of_outfits/")
     url = models.SlugField(max_length=160, unique=True)
 
     def __str__(self):
@@ -16,8 +16,7 @@ class Outfit(models.Model):
     category = models.ForeignKey(
         Category, verbose_name='Категория', on_delete=models.CASCADE)
     photo = models.ImageField("Изображение", upload_to="outfits/")
-    name = models.CharField('Название', max_length=34,
-                            default='Default Outfit')
+    name = models.CharField('Название', max_length=34, default='Default Outfit')
 
     def __str__(self):
         return self.name
@@ -28,3 +27,6 @@ class Thing(models.Model):
         Outfit, on_delete=models.CASCADE, verbose_name='Аутфит')
     photo = models.ImageField("Изображение", upload_to="things/")
     name = models.CharField('Название', max_length=34, default='Default Thing')
+
+    def __str__(self):
+        return self.name
